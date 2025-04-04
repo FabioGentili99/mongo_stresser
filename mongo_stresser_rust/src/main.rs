6e08fe07b23a6a1729f275f5eb69f9cab0ef836f
FabioGentili99 <fabiogentili20@gmail.com>
2025-04-04 14:23:29 +0200

    optimization

--- mongo_stresser_rust/src/main.rs
+++ mongo_stresser_rust/src/main.rs
@@ -1,4 +1,5 @@
 use log::info;
+use mongodb::options::FindOneOptions;
 use mongodb::{bson::doc, options::ClientOptions, Client};
 use std::process::exit;
 use std::sync::Arc;
@@ -10,16 +11,20 @@ use tokio::time::{sleep, Duration};
 async fn main() {
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
 
-    let uri = "mongodb://admin:password@192.168.17.118:27017"; // Change if necessary
+    //let uri = "mongodb://admin:password@192.168.17.118:27017"; // Change if necessary
+    let uri = "mongodb://localhost:27017";
     let db_name = "services";
     let collection_name = "services";
     let document_id = "acl"; // Change as needed
 
     // Initialize MongoDB client
-    let client_options = ClientOptions::parse(uri).await.expect("Failed to parse options");
+    let mut client_options = ClientOptions::parse(uri).await.expect("Failed to parse options");
+    client_options.direct_connection = Some(true);
+    client_options.max_pool_size = Some(10);
     let client = Client::with_options(client_options).expect("Failed to create client");
     let collection = client.database(db_name).collection::<mongodb::bson::Document>(collection_name);
     let collection = Arc::new(collection);
+
 
     let mut num_tasks = 1;
     loop {
@@ -28,6 +33,9 @@ async fn main() {
         
         for _ in 0..num_tasks {
             let collection_clone = Arc::clone(&collection);
+            let find_options = FindOneOptions::builder()
+                .sort(doc! { "$natural": -1 }) // Helps keep document in cache
+                .build();
             let handle = task::spawn(async move {
 
 		for _ in 0..100{
@@ -35,7 +43,7 @@ async fn main() {
                 // Start the timer
                 let retrieval_start = SystemTime::now();
 
-                match collection_clone.find_one(filter).await {
+                match collection_clone.find_one(filter).with_options(find_options.clone()).await {
                     Ok(Some(doc)) => println!("Read document: {:?}", doc),
                     Ok(None) => println!("Document not found"),
                     Err(e) => eprintln!("Error reading document: {}", e),
